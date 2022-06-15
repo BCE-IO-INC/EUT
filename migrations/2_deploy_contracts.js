4de52a47375c7e8156c0b7dc613f208174d2be5d
cd606 <cd606dc@gmail.com>
2022-06-15 16:26:27 -0500

    deploy the contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,27 @@
 var EutMusic = artifacts.require("./EutMusic.sol");
+var BCEMusicSettings = artifacts.require("./BCEMusicSettings.sol");
+var BCEMusicAuction = artifacts.require("./BCEMusicAuction.sol");
+var BCEMusicOffer = artifacts.require("./BCEMusicOffer.sol");
+var BCEMusic = artifacts.require("./BCEMusic.sol");
+var BCEMusicMarket = artifacts.require("./BCEMusicMarket.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(EutMusic, "EUT", 2330298);
+  deployer.deploy(BCEMusicSettings).then(
+    () => {
+      deployer.deploy(BCEMusicAuction).then(
+        () => {
+          deployer.deploy(BCEMusicOffer).then(
+            () => {
+              deployer.link(BCEMusicAuction, BCEMusic);
+              deployer.link(BCEMusicOffer, BCEMusic);
+              deployer.deploy(BCEMusic, "https://localhost:12345", BCEMusicSettings.address);
+            }
+          )
+        }
+      )
+    }
+  );
+  deployer.deploy(BCEMusicMarket);
+  
 };
