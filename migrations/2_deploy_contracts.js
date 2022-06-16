dbaa0628785cdee8a3cb521b544a5c50779ebd5a
cd606 <cd606dc@gmail.com>
2022-06-15 22:17:02 -0500

    change migrate syntax

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,17 @@
-var EutMusic = artifacts.require("./EutMusic.sol");
+//var EutMusic = artifacts.require("./EutMusic.sol");
 var BCEMusicSettings = artifacts.require("./BCEMusicSettings.sol");
 var BCEMusicAuction = artifacts.require("./BCEMusicAuction.sol");
 var BCEMusicOffer = artifacts.require("./BCEMusicOffer.sol");
 var BCEMusic = artifacts.require("./BCEMusic.sol");
 var BCEMusicMarket = artifacts.require("./BCEMusicMarket.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(EutMusic, "EUT", 2330298);
-  deployer.deploy(BCEMusicSettings).then(
-    () => {
-      deployer.deploy(BCEMusicAuction).then(
-        () => {
-          deployer.deploy(BCEMusicOffer).then(
-            () => {
-              deployer.link(BCEMusicAuction, BCEMusic);
-              deployer.link(BCEMusicOffer, BCEMusic);
-              deployer.deploy(BCEMusic, "https://localhost:12345", BCEMusicSettings.address);
-            }
-          )
-        }
-      )
-    }
-  );
-  deployer.deploy(BCEMusicMarket);
-  
+module.exports = async function(deployer) {
+  //deployer.deploy(EutMusic, "EUT", 2330298);
+  await deployer.deploy(BCEMusicSettings);
+  await deployer.deploy(BCEMusicMarket);
+  await deployer.deploy(BCEMusicAuction);
+  await deployer.deploy(BCEMusicOffer);
+  await deployer.link(BCEMusicAuction, BCEMusic);
+  await deployer.link(BCEMusicOffer, BCEMusic);
+  await deployer.deploy(BCEMusic, "https://localhost:12345", BCEMusicSettings.address);
 };
