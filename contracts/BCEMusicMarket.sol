31297dce027d4dc97c8bb7808f27214e0b395b4f
cd606 <cd606dc@gmail.com>
2022-06-18 10:59:34 -0500

    add comment

--- contracts/BCEMusicMarket.sol
+++ contracts/BCEMusicMarket.sol
@@ -3,9 +3,18 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//The current design is that BCEMusic will be deployed in various instances,
+//each instance holding diamond/golden NFTs for one single song only, and therefore
+//we use this BCEMusicMarket as a registry to hold the information for all
+//deployed instances of BCEMusic.
+
+//An alternative design where BCEMusic holds all the NFTs for all songs is 
+//technically feasible, but for now we use this design to make it easier to 
+//isolate each song (in order to diversity attack and bug risks).
+
 //If we try to create BCEMusic within a method here, solidity will complain
 //that the contract size is too big for deployment onto mainnet, therefore
-//this contract is simply a registry.
+//this contract is simply a name registry.
 
 contract BCEMusicMarket is Ownable {
     mapping (string => address) private _musicNFTs;
