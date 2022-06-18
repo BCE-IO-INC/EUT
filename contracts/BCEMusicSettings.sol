58b88f9f959b2ed28d3d2a3033c907a4ac756d0f
cd606 <cd606dc@gmail.com>
2022-06-18 11:02:12 -0500

    reduce settings storage

--- contracts/BCEMusicSettings.sol
+++ contracts/BCEMusicSettings.sol
@@ -5,25 +5,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IBCEMusicSettings.sol";
 
 contract BCEMusicSettings is Ownable, IBCEMusicSettings {
-    uint private _ownerFeePercentForAuction;
-    uint private _ownerFeePercentForSecondaryMarket;
+    uint8 private _ownerFeePercentForAuction;
+    uint8 private _ownerFeePercentForSecondaryMarket;
 
     constructor() Ownable() {
         _ownerFeePercentForAuction = 10;
-        _ownerFeePercentForSecondaryMarket = 5;
+        _ownerFeePercentForSecondaryMarket = 1;
     }
-    function setOwnerFeePercentForAuction(uint p) external onlyOwner {
+    function setOwnerFeePercentForAuction(uint8 p) external onlyOwner {
         require(p>=0 && p<=100, "bad percentage.");
         _ownerFeePercentForAuction = p;
     }
-    function setOwnerFeePercentForSecondaryMarket(uint p) external onlyOwner {
+    function setOwnerFeePercentForSecondaryMarket(uint8 p) external onlyOwner {
         require(p>=0 && p<=100, "bad percentage.");
         _ownerFeePercentForSecondaryMarket = p;
     }
-    function ownerFeePercentForAuction() external view override returns (uint) {
+    function ownerFeePercentForAuction() external view override returns (uint8) {
         return _ownerFeePercentForAuction;
     }
-    function ownerFeePercentForSecondaryMarket() external view override returns (uint) {
+    function ownerFeePercentForSecondaryMarket() external view override returns (uint8) {
         return _ownerFeePercentForSecondaryMarket;
     }
 }
--- contracts/IBCEMusicSettings.sol
+++ contracts/IBCEMusicSettings.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.4;
 
 interface IBCEMusicSettings {
-    function ownerFeePercentForAuction() external view returns (uint);
-    function ownerFeePercentForSecondaryMarket() external view returns (uint);
+    function ownerFeePercentForAuction() external view returns (uint8);
+    function ownerFeePercentForSecondaryMarket() external view returns (uint8);
 }
