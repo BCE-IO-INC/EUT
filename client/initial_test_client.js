4a251e5c616a27c5eec1a5146a8a869d7fbaf0d3
omniglot-llc-il <omniglotllc@gmail.com>
2022-06-27 02:11:55 +0000

    test building up ownership info

--- client/initial_test_client.js
+++ client/initial_test_client.js
@@ -2,6 +2,7 @@ const Web3 = require('web3');
 const fs = require('fs');
 const contract = require("@truffle/contract");
 const util = require('node:util');
+const _ = require('lodash');
 
 const settingsContractArtifact = JSON.parse(fs.readFileSync(__dirname+"/../build/contracts/BCEMusicSettings.json")); //produced by Truffle compile
 const SettingsContract = contract(settingsContractArtifact);
@@ -13,6 +14,8 @@ const provider = new Web3.providers.WebsocketProvider("ws://localhost:8545");
 const web3 = new Web3(provider);
 SettingsContract.setProvider(provider);
 MusicContract.setProvider(provider);
+
+var ownership = {};
 
 (async () => {
     const settingsContract = await SettingsContract.deployed();
@@ -34,10 +37,32 @@ MusicContract.setProvider(provider);
                 console.log(`withdrawing offer ${event.returnValues.offerId}`);
                 musicContract.withdrawOffer(event.returnValues.tokenId, event.returnValues.offerId, {from: accts[0]});
             } else if (event.event === 'TransferSingle') {
+                var id = event.returnValues.id;
                 var from = event.returnValues.from;
                 var to = event.returnValues.to;
                 var value = event.returnValues.value;
-                console.log(`${event.blockNumber} transfer ${value}`);
+                if (new web3.utils.BN(from).toNumber() !== 0) {
+                    if (ownership[from] === undefined) {
+                        ownership[from] = {};
+                    }
+                    if (ownership[from][id] === undefined) {
+                        ownership[from][id] = 0;
+                    }
+                    ownership[from][id] -= parseInt(value);
+                }
+                if (ownership[to] === undefined) {
+                    ownership[to] = {};
+                }
+                if (ownership[to][id] === undefined) {
+                    ownership[to][id] = 0;
+                }
+                ownership[to][id] += parseInt(value);
+                console.log(`${event.blockNumber}:`); 
+                _.forEach(ownership, (v, k) => {
+                    _.forEach(v, (v1, k1) => {
+                        console.log(`\t${k} has ${v1} tokens of token ${k1} on address ${event.address}`);
+                    });
+                });
             }
         });
     
